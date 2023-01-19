8dbfd9ee40f9e725f908b73819458349e973cbae
ek2999 <ek2999aleksei@gmail.com>
2023-01-19 07:06:32 +0300

    primer 2

--- src/main/java/com/example/demo3/Main.java
+++ src/main/java/com/example/demo3/Main.java
@@ -1,8 +1,36 @@
 package com.example.demo3;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+//        Object o = new Scanner(System.in);
+//
+//        ((Scanner) o).nextInt(); //приведение типа
+//
+//        Scanner scanner = null;
+//
+//        if (o instanceof Scanner) scanner = (Scanner) o; //проверим что объект принадлежит классу
+//
+//        if (scanner != null) {
+//            scanner.nextInt();
+//        }
+//
+//       // ((String) o).length();
+//
+//
+//        Object o1 = new String();
+
+        Object[] objects = {10, "Hello", 3.14};
+
+        for(Object o:objects){
+            if(o instanceof String) {
+                String s = (String) o;
+                System.out.println(s);
+            }
+        }
 
     }
 
